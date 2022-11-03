7f0d62ed3f2dc1f1ebe828a946dd4d52ab5e2c24
= <hvpetrow@gmail.com>
2022-11-03 14:54:41 +0100

    updated

--- angular-and-ts-intro/first-angular-project/src/app/app.component.ts
+++ angular-and-ts-intro/first-angular-project/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'first-angular-project';
+  title = "first-angular-project";
+  showText = true;
+
+  users = {
+    gosgho: 1,
+    penco: 2
+  }
+
+  users2: boolean[] = [false, true];
+  users3: string = "Ivan";
+
+
+  toggleText(): void {
+    this.showText = !this.showText;
+
+  }
+
+
 }
