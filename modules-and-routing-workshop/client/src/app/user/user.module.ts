11f94dcb7ef671727ed7ba2a86f1c6d286b8d3e7
= <hvpetrow@gmail.com>
2022-11-11 19:44:04 +0100

    updated

--- modules-and-routing-workshop/client/src/app/user/user.module.ts
+++ modules-and-routing-workshop/client/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { UserService } from './user.service';
 
 
 
@@ -16,6 +17,9 @@ import { UserRoutingModule } from './user-routing.module';
   imports: [
     CommonModule,
     UserRoutingModule
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }
---
+++ modules-and-routing-workshop/client/src/app/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserService {
+
+  constructor() { }
+
+  login(email: string, password: string): void {
+
+  }
+
+  logout(): void {
+
+  }
+}
