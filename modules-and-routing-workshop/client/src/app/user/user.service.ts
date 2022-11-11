06de81af3a1cecc7a3ee9790ab75e281d0b7e081
= <hvpetrow@gmail.com>
2022-11-11 19:49:09 +0100

    updated

--- modules-and-routing-workshop/client/src/app/user/user.service.ts
+++ modules-and-routing-workshop/client/src/app/user/user.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
+import { IUser } from '../shared/interfaces';
 
 @Injectable()
 export class UserService {
 
+
+  user: IUser | undefined;
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
   constructor() { }
 
   login(email: string, password: string): void {
-
+    setTimeout(() => {
+      this.user = {
+        email,
+        firstName: 'John',
+        lastName: 'Doe'
+      }
+    }, 1000)
   }
 
   logout(): void {
-
+    setTimeout(() => {
+      this.user = undefined;
+    }, 1000)
   }
 }
