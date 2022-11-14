8bb0c49ac2457a9cc7ff4681d84e739ae3b911d0
= <hvpetrow@gmail.com>
2022-11-14 19:09:00 +0100

    interface added

--- modules-and-routing2/demo/src/app/customer/customer.service.ts
+++ modules-and-routing2/demo/src/app/customer/customer.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface IUser {
+  "id": 1,
+  "name": string,
+  "username": string,
+  "email": string,
+  "address": {
+    "street": string,
+    "suite": string,
+    "city": string,
+    "zipcode": string,
+    "geo": {
+      "lat": string,
+      "lng": string
+    }
+  },
+  "phone": string,
+  "website": string,
+  "company": {
+    "name": string,
+    "catchPhrase": string,
+    "bs": string
+  }
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers$(): Observable<IUser[]> {
+    return this.httpClient.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+  }
 }
