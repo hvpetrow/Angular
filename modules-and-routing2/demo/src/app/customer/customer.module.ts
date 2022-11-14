e4a42ce84ba558fa9e09721d2d249cf8795e1168
= <hvpetrow@gmail.com>
2022-11-14 18:55:33 +0100

    initialized

---
+++ modules-and-routing2/demo/src/app/customer/customer.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomerListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CustomerListComponent
+  ]
+})
+export class CustomerModule { }
