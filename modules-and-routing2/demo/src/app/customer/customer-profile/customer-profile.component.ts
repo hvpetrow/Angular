88dfb6ed83c73ebf5735386546db3ad4299a048c
= <hvpetrow@gmail.com>
2022-11-15 18:35:41 +0100

    updated

--- modules-and-routing2/demo/src/app/customer/customer-profile/customer-profile.component.ts
+++ modules-and-routing2/demo/src/app/customer/customer-profile/customer-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { tap, mergeMap } from 'rxjs';
 import { CustomerService, IUser } from '../customer.service';
 
 @Component({
@@ -9,23 +11,47 @@ import { CustomerService, IUser } from '../customer.service';
 })
 export class CustomerProfileComponent implements OnInit {
   customer!: IUser;
+  id!: number;
+  isLoading: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute, private customerService: CustomerService) { }
+  constructor(private activatedRoute: ActivatedRoute, private customerService: CustomerService, private titleService: Title) { }
 
   ngOnInit(): void {
-    //subscribe for params changes in URL 
-    this.activatedRoute.params.subscribe(params => {
-      const id = this.activatedRoute.snapshot.params['id'];
+    //subscribe for params changes in URL
+    this.activatedRoute.params.pipe(
+      tap(params => {
+        this.id = +params['id'];
 
-      this.customerService.getUserById$(id).subscribe({
+        this.titleService.setTitle('Profile ' + this.id);
+        this.isLoading = true;
+      }),
+      mergeMap(params => {
+        return this.customerService.getUserById$(params['id'])
+      })
+    )
+      .subscribe({
         next: user => {
           this.customer = user;
+          this.isLoading = false;
         },
         error: error => {
+          this.isLoading = false;
           console.error('error happened', error);
         }
-      })
-    })
+      }
+      )
+    // this.activatedRoute.params.subscribe(params => {
+    //   const id = params['id'];
+
+    //   this.customerService.getUserById$(id).subscribe({
+    //     next: user => {
+    //       this.customer = user;
+    //     },
+    //     error: error => {
+    //       console.error('error happened', error);
+    //     }
+    //   })
+    // })
 
     // this.customerService.getUserById$(id).subscribe(user => {
     //   console.log(user);
