976d4972ecb259eb2576405271033be8cf3fc4ce
= <hvpetrow@gmail.com>
2022-11-15 16:28:30 +0100

    updatred

--- modules-and-routing2/demo/src/app/app.module.ts
+++ modules-and-routing2/demo/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CustomerModule
+    CustomerModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
