bf66660d9e1ef423930a2f23ae820c8bedbc9e83
= <hvpetrow@gmail.com>
2022-11-15 17:17:37 +0100

    updated

--- modules-and-routing2/demo/src/app/customer/customer.module.ts
+++ modules-and-routing2/demo/src/app/customer/customer.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerListItemComponent } from './customer-list-item/customer-list-item.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     CustomerListComponent,
-    CustomerListItemComponent
+    CustomerListItemComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     CustomerListComponent
