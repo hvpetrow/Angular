6c37d6846bfce82dcb7cb7ef651403a50717f32e
= <hvpetrow@gmail.com>
2022-11-17 17:09:47 +0100

    updated

--- forms/forms/src/app/directives-demo/directives-demo.component.ts
+++ forms/forms/src/app/directives-demo/directives-demo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directives-demo.component.css']
 })
 export class DirectivesDemoComponent implements OnInit {
-  shouldFontBeBig: boolean = true;
+  shouldFontBeBig: boolean = false;
 
   constructor() { }
 
