dae223d772fbbe16558da408abd28d50d5029b9d
= <hvpetrow@gmail.com>
2022-11-18 16:39:36 +0100

    forms module added

--- forms/forms/src/app/app.module.ts
+++ forms/forms/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { TemplateFormsDemoComponent } from './template-forms-demo/template-forms
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
