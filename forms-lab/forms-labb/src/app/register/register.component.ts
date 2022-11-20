76c1185bbd336dcd6888cec5ae25253bfcf14b2d
= <hvpetrow@gmail.com>
2022-11-20 11:17:31 +0100

    template form implemented

--- forms-lab/forms-labb/src/app/register/register.component.ts
+++ forms-lab/forms-labb/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  @ViewChild('registerForm') registerForm!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.registerForm.value);
   }
 
 }
