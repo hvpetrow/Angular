1e64c8f62004148b2d59a9d61ed7df994a62e9f6
= <hvpetrow@gmail.com>
2022-11-22 18:17:08 +0100

    updated

--- forms-workshop/forms-workshop/src/app/auth/register/register.component.ts
+++ forms-workshop/forms-workshop/src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { emailValidator } from '../util';
+import { emailValidator, passwordMatch } from '../util';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +9,16 @@ import { emailValidator } from '../util';
 })
 export class RegisterComponent implements OnInit {
 
+  passwordControl = new FormControl(null, [Validators.required, Validators.minLength(4)])
+
   registerFormGroup: FormGroup = this.formBuilder.group({
     'username': new FormControl(null, [Validators.required, Validators.minLength(5)]),
     'email': new FormControl(null, [Validators.required, emailValidator]),
     'passwords': new FormGroup({
-      'password': new FormControl(null, [Validators.required, Validators.minLength(4)]),
-      'rePassword': new FormControl(null, Validators.required,)
+      'password': new FormControl(this.passwordControl),
+      'rePassword': new FormControl(null, [passwordMatch(this.passwordControl)])
     }),
+    'telRegion': new FormControl(null, []),
     'tel': new FormControl(null, [])
   });
 
--- forms-workshop/forms-workshop/src/app/auth/util.ts
+++ forms-workshop/forms-workshop/src/app/auth/util.ts
@@ -3,10 +3,26 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export function emailValidator(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
 
+    if (!value) {
+        return null;
+    }
+
     if (!/.{6,}@gmail\.(bg|com)/.test(value)) {
         return {
             email: true
         }
     }
     return null;
+}
+
+export function passwordMatch(passwordFormControl: AbstractControl) {
+    return (rePasswordFormControl: AbstractControl) => {
+        if (passwordFormControl.value !== rePasswordFormControl.value) {
+            return {
+                passwordMatch: true
+            }
+        }
+
+        return null;
+    }
 }
