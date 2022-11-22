40cf0ba8089946fdcb9f720b299ef6d5dfebbe3a
= <hvpetrow@gmail.com>
2022-11-22 19:29:52 +0100

    updated

--- forms-workshop/forms-workshop/src/app/auth/register/register.component.ts
+++ forms-workshop/forms-workshop/src/app/auth/register/register.component.ts
@@ -9,7 +9,7 @@ import { emailValidator, passwordMatch } from '../util';
 })
 export class RegisterComponent implements OnInit {
 
-  passwordControl = new FormControl(null, [Validators.required, Validators.minLength(4)]);
+  passwordControl = new FormControl(null, [Validators.required, Validators.minLength(5)]);
 
   get passwordsGroup(): FormGroup {
     return this.registerFormGroup.controls.passwords as FormGroup;
@@ -19,8 +19,8 @@ export class RegisterComponent implements OnInit {
     'username': new FormControl(null, [Validators.required, Validators.minLength(5)]),
     'email': new FormControl(null, [Validators.required, emailValidator]),
     'passwords': new FormGroup({
-      'password': new FormControl(this.passwordControl),
-      'rePassword': new FormControl(null, [passwordMatch(this.passwordControl)])
+      'password': this.passwordControl,
+      'rePassword': new FormControl(null, [Validators.required, passwordMatch(this.passwordControl)])
     }),
     'telRegion': new FormControl(null, []),
     'tel': new FormControl(null, [])
@@ -37,7 +37,7 @@ export class RegisterComponent implements OnInit {
   }
 
   shouldShowErrorForControl(controlName: string, sourceGroup: FormGroup = this.registerFormGroup) {
-    return sourceGroup.controls[controlName].touched && this.passwordsGroup.controls[controlName].invalid;
+    return sourceGroup.controls[controlName].touched && sourceGroup.controls[controlName].invalid;
   }
 
 }
