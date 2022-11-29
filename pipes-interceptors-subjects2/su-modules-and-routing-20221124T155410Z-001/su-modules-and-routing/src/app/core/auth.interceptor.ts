6cb0922e4900b57bd977b80558ee38a4385efb08
= <hvpetrow@gmail.com>
2022-11-29 16:30:20 +0100

    updated

---
+++ pipes-interceptors-subjects2/su-modules-and-routing-20221124T155410Z-001/su-modules-and-routing/src/app/core/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { IUser } from '../customer/customer.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(tap(event => {
+      if (event instanceof HttpResponse) {
+        if (event.url?.endsWith('login') || event.url?.endsWith('register')) {
+          const newlyLoggedUser = event.body;
+        }
+      }
+    }));
+  }
+}
