9f6669966d910dc8df51820651700719b076b174
= <hvpetrow@gmail.com>
2022-12-01 18:35:19 +0100

    initialized

---
+++ forms-workshop/forms-workshop/src/app/shared/my-counter/my-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css']
+})
+export class MyCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
