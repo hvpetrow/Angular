a410c06f58fdf074c3b8114a89913d95c061f29f
= <hvpetrow@gmail.com>
2022-12-02 19:21:02 +0100

    login route added

--- armwrestlingbook/client/src/app/app.module.ts
+++ armwrestlingbook/client/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './authentication/login/login.component';
+import { LoginModule } from './authentication/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
