dfd7f76d83d25b0e38e2dc0d0b93c15be85cf31b
= <hvpetrow@gmail.com>
2022-12-03 18:30:36 +0100

    router Module added

--- armwrestlingbook/client/src/app/app-routing.module.ts
+++ armwrestlingbook/client/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
 
 ];
 
